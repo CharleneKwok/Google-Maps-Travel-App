d3efc4df8c5bd43ed28261eef94dc3e9dd4efd02
Charlene Kwok <charlenekwok499@gmail.com>
2021-11-05 18:37:56 +1100

    add style

--- travel_app/src/App.js
+++ travel_app/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+// normalising the styles
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";;
 
 const App = () => {
     return (
-        <div>
-            <h1>hello world!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    {/* 
+                        xs => full width on mobile devices
+                        md => medium device only 
+                    */}
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
